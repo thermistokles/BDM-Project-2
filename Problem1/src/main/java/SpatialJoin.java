6d7671e9d2fb1cac438e8269ae911fd2f1b9fa15
Will Burke <wgburke@wpi.edu>
2022-11-08 16:24:27 -0500

    Setting up hadoop project correctly

---
+++ Problem1/src/main/java/SpatialJoin.java
@@ -0,0 +1,10 @@
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+import java.util.*;
+
+public class SpatialJoin {
+    public static void print(){
+        System.out.println("Ah");
+    }
+}
