94a606745c7fd31630e8358902c0a13e7c99d08d
Will Burke <wgburke@wpi.edu>
2022-11-11 16:25:19 -0500

    Added part where we have to save to csv

--- Problem3/main.py
+++ Problem3/main.py
@@ -37,7 +37,7 @@ T2 = spark.sql("SELECT transNumItems, sum(transTotal) as sum, avg(transTotal) as
 # T2.orderBy("transNumItems").show(10)
 
 # 3 - Report back T2 to the client side
-# ?????
+T2.write.csv("data/T2")
 
 # 4 - T3: Over T1, group the transactions by customer ID, and for each group report the
 # customer ID and the transactions’ count
@@ -59,13 +59,13 @@ T5 = spark.sql("SELECT custID, count(ALL transID) as numTrans FROM transactions 
 # 7 - T6: Select the customer IDs whose T5.count * 5 < T3.count
 T3.createOrReplaceTempView("T3")
 T5.createOrReplaceTempView("T5")
-T6 = spark.sql("SELECT T5.custID, T5.numTrans, T3.numTrans "
-               "FROM T5 join T3 ON T5.custID=T3.custID ")
-               # "WHERE T5.numTrans5 < T3.numTrans3")
-T6.orderBy("custID").show(20)
+T6 = spark.sql("SELECT T5.custID, T5.numTrans as trans2, T3.numTrans as trans1 "
+               "FROM T5 join T3 ON T5.custID=T3.custID "
+               "WHERE T5.numTrans < T3.numTrans")
+# T6.orderBy("custID").show(20)
 
 # 8 - Report back T6 to the client side
-# ?????
+T6.write.csv("data/T6")
 
 # Stopping Spark session
 spark.stop()
